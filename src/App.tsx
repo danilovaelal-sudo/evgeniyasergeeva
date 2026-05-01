37bb64a79bae7bbbdb22aabeadbd43797487d0f2
danilovaelal-sudo <danilovaelal@gmail.com>
2026-05-01 20:53:27 +0300

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/evgeniyasergeeva">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/privacy" element={<Privacy />} />
